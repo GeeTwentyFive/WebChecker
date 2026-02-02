6b42ab4bc9497bf4cfc13c31127bff31b770d0bc
Gee <alickoamarko@gmail.com>
2026-02-02 14:51:42 +0100

    MVP?

--- WebChecker.py
+++ WebChecker.py
@@ -14,10 +14,13 @@ CONFIG_FILE_NAME = "WebChecker_config.json"
 DEFAULT_CONFIG = {
 	"target_websites": [],
 	"webchecker_email_server": "smtp.gmail.com",
+	"webchecker_email_server_port": 465,
 	"webchecker_email_address": "",
 	"webchecker_email_password": "",
 	"alert_email_addresses": [],
-	"check_interval": 3600.0
+	"openai_api_key": "",
+	"per_website_check_interval": 60.0,
+	"all_checks_interval": 3600.0
 }
 
 
@@ -43,29 +46,51 @@ if len(config["alert_email_addresses"]) == 0:
 	sys.exit(1)
 
 
-email_server = smtplib.SMTP_SSL(config["webchecker_email_server"], 465, context=ssl.create_default_context())
+print("Logging in to mailserver...")
+email_server = smtplib.SMTP_SSL(
+	config["webchecker_email_server"],
+	config["webchecker_email_server_port"],
+	context=ssl.create_default_context()
+)
 email_server.login(config["webchecker_email_address"], config["webchecker_email_password"])
 
-openai_client = OpenAI() # TODO
+print("Requesting OpenAI session...")
+openai_client = OpenAI(api_key=config["openai_api_key"])
 
+print("Running")
 while True:
 	for target_website in config["target_websites"]:
 		try: uo = urllib.request.urlopen(target_website)
-		except:
-			print(f"ERROR: Failed to fetch HTML from {target_website}")
+		except Exception as e:
+			print(f"ERROR: Failed to fetch HTML from {target_website}\n{str(e)}")
 			try: email_server.sendmail(
 				config["webchecker_email_address"],
 				config["alert_email_addresses"],
 				f"Website '{target_website}' might be down"
 			)
-			except:
-				print("ERROR: Failed to send down alert email")
-				sys.exit(1)
+			except Exception as e: print(f"ERROR: Failed to send down alert email\n{str(e)}")
 			continue
-		
+
 		soup = BeautifulSoup(uo.read(), features="html.parser")
 		for elem in soup(["script", "style"]): elem.decompose()
 
-		# TODO: `soup.get_text()` -> check with AI, e-mail if undesireable content
+		try: response = openai_client.moderations.create(
+			model="omni-moderation-latest",
+			input=soup.get_text()
+		)
+		except Exception as e:
+			print(f"ERROR: Failed to get response from OpenAI\n{str(e)}")
+			continue
 
-	time.sleep(config["check_interval"])
+		for result in response["results"]:
+			if result["flagged"] == True:
+				try: email_server.sendmail(
+					config["webchecker_email_address"],
+					config["alert_email_addresses"],
+					f"Potentially inappropriate content found on '{target_website}':\n{json.dumps(result)}"
+				)
+				except Exception as e: print(f"ERROR: Failed to send potential inappropriate content alert email\n{str(e)}")
+		
+		time.sleep(config["per_website_check_interval"])
+
+	time.sleep(config["all_checks_interval"])
